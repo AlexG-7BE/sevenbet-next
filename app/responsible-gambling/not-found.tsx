77689c34d96240632f41149d20c6b02f7c71487c
Aleksandr Gorlenko <gorlenko.aleks@gmail.com>
2026-08-07 23:01:23 +0500

    FE-GAP-02: migrate protected help articles

--- app/responsible-gambling/not-found.tsx
+++ app/responsible-gambling/not-found.tsx
@@ -1,19 +1,5 @@
-import Link from "next/link";
+import { ProtectedHelpArticleUnavailable } from "@/components/protected-help/ProtectedHelpArticle";
 
 export default function ProtectedHelpNotFound() {
-  return (
-    <section className="pageShell">
-      <div className="container">
-        <div className="card discoveryEmpty">
-          <p className="eyebrow">Help page not found</p>
-          <h1>This support page is unavailable.</h1>
-          <p>The link may be outdated. You can return to the Help centre or leave this section.</p>
-          <div className="heroActions">
-            <Link className="button gold" href="/responsible-gambling">Return to Help</Link>
-            <Link className="button ghost" href="/">Go home</Link>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  return <ProtectedHelpArticleUnavailable />;
 }
